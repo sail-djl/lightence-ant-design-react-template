a1f67855f3c306f8d87f763c6519c79a6d3d9eec
Sail <jialong158790@qq.com>
2025-12-02 12:09:04 +0800

    feat: 平台信息

--- src/components/common/PageTitle/PageTitle.tsx
+++ src/components/common/PageTitle/PageTitle.tsx
@@ -5,7 +5,7 @@ import { Helmet } from 'react-helmet-async';
 export const PageTitle: React.FC<WithChildrenProps> = ({ children }) => {
   return (
     <Helmet>
-      <title>{children} | Lightence Admin</title>
+      <title>{children} | 航融</title>
     </Helmet>
   );
 };
